a1f68a88b8e6fcd8cc7c1ec33d2bcc9b051755f8
Wojciech Kusa <wojciech.kusa@gmail.com>
2023-01-31 09:11:53 +1000

    feat(flair): move training into function

--- src/models/phase1/train_flair.py
+++ src/models/phase1/train_flair.py
@@ -9,67 +9,76 @@ import os
 import argparse
 
 
-if __name__ == '__main__':
+def train_ner_model(
+    data_folder="data/interim/flair/",
+    train_file="st1_train_flair.txt",
+    dev_file="st1_val_flair.txt",
+    model_name="distilbert-base-uncased",
+    model_output_file="data/results/taggers/flair-distilbert",
+):
+    columns = {0: "text", 1: "ner"}
+    label_type = "ner"
+
+    corpus: Corpus = ColumnCorpus(
+        data_folder,
+        columns,
+        train_file=train_file,
+        dev_file=dev_file,
+    )
+
+    label_dict = corpus.make_label_dictionary(label_type=label_type)
+    print(train_file, label_dict)
+
+    embeddings = TransformerWordEmbeddings(
+        model=model_name,
+        layers="-1",
+        subtoken_pooling="first",
+        fine_tune=True,
+        use_context=True,
+    )
+
+    tagger = SequenceTagger(
+        hidden_size=256,
+        embeddings=embeddings,
+        tag_dictionary=label_dict,
+        tag_type="ner",
+        use_crf=True,
+        use_rnn=False,
+        reproject_embeddings=False,
+    )
+
+    trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+    trainer.fine_tune(
+        model_output_file,
+        learning_rate=5.0e-4,
+        max_epochs=30,
+        mini_batch_size=32,
+    )
+
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="distilbert-base-uncased")
-    parser.add_argument("--results_path", type=str, default="results/flair/")
+    parser.add_argument("--data_path", type=str, default="data/interim/flair/")
+    parser.add_argument(
+        "--train_dataset_file", type=str, default="st1_train_flair.txt"
+    )
+    parser.add_argument("--test_dataset_file", type=str, default="st1_val_flair.txt")
+    parser.add_argument(
+        "--model_output_file", type=str, default="data/results/taggers/flair-distilbert"
+    )
 
     args = parser.parse_args()
 
-    os.environ['KMP_DUPLICATE_LIB_OK']='True' # fixme: remove this line
+    if not os.path.exists(args.model_output_file):
+        os.makedirs(args.model_output_file)
 
-    # define columns
-    columns = {0: 'text', 1: 'ner'}
-
-    # this is the folder in which train, test and dev files reside
-    data_folder = 'data/interim/flair/'
-
-    corpus: Corpus = ColumnCorpus(data_folder, columns,
-                                  train_file='st1_train_flair.txt',
-                                  dev_file='st1_train_flair.txt')
-
-    print(len(corpus.train))
-    print(corpus.train[0].to_tagged_string('ner'))
-
-
-    # 2. what label do we want to predict?
-    label_type = 'ner'
-
-    # 3. make the label dictionary from the corpus
-    label_dict = corpus.make_label_dictionary(label_type=label_type)
-    print(label_dict)
-
-
-
-    model_name = "distilbert-base-uncased"
-
-    # 4. initialize fine-tuneable transformer embeddings WITH document context
-    embeddings = TransformerWordEmbeddings(model=model_name,
-                                           layers="-1",
-                                           subtoken_pooling="first",
-                                           fine_tune=True,
-                                           use_context=True,
-                                           )
-
-    # 5. initialize bare-bones sequence tagger (no CRF, no RNN, no reprojection)
-    tagger = SequenceTagger(hidden_size=256,
-                            embeddings=embeddings,
-                            tag_dictionary=label_dict,
-                            tag_type='ner',
-                            use_crf=True,
-                            use_rnn=False,
-                            reproject_embeddings=False,
-                            )
-
-    # 6. initialize trainer
-    trainer: ModelTrainer = ModelTrainer(tagger, corpus)
-
-
-    # 7. run fine-tuning
-    trainer.fine_tune('data/results/taggers/flair-distilbert',
-                      learning_rate=5.0e-4,
-                      max_epochs=30,
-                      mini_batch_size=32,
-                      mini_batch_chunk_size=1,  # remove this parameter to speed up computation if you have a big GPU
-                      )
+    train_ner_model(
+        data_folder=args.data_path,
+        train_file=args.train_dataset_file,
+        dev_file=args.test_dataset_file,
+        model_name=args.model_name,
+        model_output_file=args.model_output_file,
+    )
